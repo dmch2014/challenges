add6569a0c8003b25bd13aa4d5562e705aeac22f
Desire <desirechiquito@gmail.com>
2023-12-07 12:57:57 +0100

    minor changes

--- divide_or_square.py
+++ divide_or_square.py
@@ -3,7 +3,7 @@
 # argument (a number), and returns the square root of the number
 # if it is divisible by 5, returns its remainder if it is not divisible by
 # 5. For example, if you pass 10 as an argument, then your function
-# should return 3.16 as the square root.
+# should return 3.16 as the square root
 import math
 def divide_or_square(param_number):
     #the reminder operator it's used to check divisibility of a number with 5
