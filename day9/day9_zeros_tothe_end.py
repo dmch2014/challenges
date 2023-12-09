be199a37c7975dc9d5b0c45fa1430add204e808a
Desire <desirechiquito@gmail.com>
2023-12-09 17:20:23 +0100

    dia 3-2

---
+++ day9/day9_zeros_tothe_end.py
@@ -0,0 +1,27 @@
+# Write a function called zeros_last. This function takes a list as
+# argument. If a list has zeros (0), it will move them to the front of
+# the list and maintain the order of the other numbers in the list.
+# If there are no Zeros in the list, the function should return the
+# original list sorted in ascending order. For example, if you pass
+# [1, 4, 6, 0, 7,0,9] as an argument,
+# your code should return [1,4, 6, 7, 9, 0, 0]. If you pass [2, 1, 4, 7, 6] as your argument,
+# your code should return [1, 2, 4, 6, 7].
+
+def zeros_to_the_end(list_of_numbers):
+    if 0 in list_of_numbers:
+        original_length = len(list_of_numbers)
+        list_excluding_zeros = [elem for elem in list_of_numbers if elem != 0]
+        how_many_zeros_to_add = original_length - len(list_excluding_zeros)
+        print(list_excluding_zeros)
+        print(how_many_zeros_to_add)
+        # ordeno la lista sin ceros
+        list_excluding_zeros= sorted(list_excluding_zeros)
+        #addinng zeros again by extendinng the list
+        list_excluding_zeros.extend([0 for i in range(how_many_zeros_to_add)])
+        listed_sorted_zeros_in_front = list_excluding_zeros
+        return listed_sorted_zeros_in_front
+    else:
+        return sorted(list_of_numbers)
+
+
+
