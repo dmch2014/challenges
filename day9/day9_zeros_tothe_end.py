1e24ea88a733352d6e9066d199f288d29179da80
Desire <desirechiquito@gmail.com>
2023-12-09 17:26:50 +0100

    dia 3-3

--- day9/day9_zeros_tothe_end.py
+++ day9/day9_zeros_tothe_end.py
@@ -6,6 +6,7 @@
 # [1, 4, 6, 0, 7,0,9] as an argument,
 # your code should return [1,4, 6, 7, 9, 0, 0]. If you pass [2, 1, 4, 7, 6] as your argument,
 # your code should return [1, 2, 4, 6, 7].
+# este reto es mejorable
 
 def zeros_to_the_end(list_of_numbers):
     if 0 in list_of_numbers:
@@ -15,8 +16,8 @@ def zeros_to_the_end(list_of_numbers):
         print(list_excluding_zeros)
         print(how_many_zeros_to_add)
         # ordeno la lista sin ceros
-        list_excluding_zeros= sorted(list_excluding_zeros)
-        #addinng zeros again by extendinng the list
+        list_excluding_zeros = sorted(list_excluding_zeros)
+        # addinng zeros again by extendinng the list
         list_excluding_zeros.extend([0 for i in range(how_many_zeros_to_add)])
         listed_sorted_zeros_in_front = list_excluding_zeros
         return listed_sorted_zeros_in_front
@@ -24,4 +25,12 @@ def zeros_to_the_end(list_of_numbers):
         return sorted(list_of_numbers)
 
 
+# version 3 using only list comprenhension
+def zeros_to_the_end_improved(list_of_numbers):
+    zeros = [num for num in list_of_numbers if num == 0]
+    non_zeros = [num for num in list_of_numbers if num != 0]
 
+    if zeros:
+        return zeros + non_zeros
+    else:
+        return sorted(list_of_numbers)
