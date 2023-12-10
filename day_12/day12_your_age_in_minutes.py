c1d84d1cadd39925af7ee412d0efe80dfe4058eb
Desire <desirechiquito@gmail.com>
2023-12-10 12:31:24 +0100

    dia 4-3

--- day_12/day12_your_age_in_minutes.py
+++ day_12/day12_your_age_in_minutes.py
@@ -1,10 +1,23 @@
 import datetime
 def your_age_in_minutes():
-    year_birth=int(input("Tell us your year:"))
-    today = datetime.date.today()
-    current_year = today.year
-    print(current_year)
-    difference_years=current_year-year_birth
-    minutes= difference_years * 365 * 24 * 60
-    print(minutes)
+    while True:
+
+            year_birth=input("Tell us your year:")
+            year_birth_num= int(year_birth)
+
+            if (year_birth_num < 1900 or len(year_birth)>4):
+                print("Sorry, this year it's not a valid one...try again")
+                continue
+            else:
+                #we're happy with the value given.
+                #we're ready to exit the loop.
+                today = datetime.date.today()
+                current_year = today.year
+                difference_years=current_year-year_birth_num
+                minutes= difference_years * 365 * 24 * 60
+                return print("Your age in minutes is:", minutes)
+                break
+
+
+
 
