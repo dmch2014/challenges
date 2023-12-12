1e20321d6c26103e3d5670d7c4f5122efea8d5f8
Desire <desirechiquito@gmail.com>
2023-12-12 19:23:58 +0100

    Day 6-4

---
+++ day_15/day15_reverse_string.py
@@ -0,0 +1,18 @@
+# Write a function called same_in_reverse that takes a string
+# and checks if the string reads the same in reverse. If it is the
+# same, the code should return True if not, it should return False.
+# For example, ‘dad’ should return True, because it reads the
+# same in reverse.
+#Create a slice that starts at the end of the string, and moves backwards.
+
+
+
+def same_in_reverse(word_string):
+    #In this particular example, the slice statement [::-1] means start at the end of the string and end at position 0,
+    #move with the step -1, negative one, which means one step backwards.
+    reverse_string = word_string[::-1]
+    if reverse_string==word_string:
+        print(True)
+    else:
+        print(False)
+    print(reverse_string)
