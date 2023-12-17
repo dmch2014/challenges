04d67ea723fe5145afff5fe1e72e05fe465cab6f
Desire <desirechiquito@gmail.com>
2023-12-17 10:27:21 +0100

    day 10-2

--- day_18/day18_add_adn_reverse.py
+++ day_18/day18_add_adn_reverse.py
@@ -7,10 +7,13 @@
 
 
 def add_reverse(list1 , list2):
-    reversed_list = []
+
     if len(list1) == len(list2):
+        reversed_list = []
         for i, val in enumerate(list1):
             reversed_list.append(list1[i]+list2[i])
+
     else:
         print("the lists are not of equal lengths")
-    return list(reversed(reversed_list))
+    #return list(reversed(reversed_list))
+    return reversed_list[::-1]
